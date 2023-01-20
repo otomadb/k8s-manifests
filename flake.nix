925767ffde18c081fdfe834cb10eb834791b5118
SnO2WMaN <me@sno2wman.net>
2023-01-21 01:28:49 +0900

    setup nix

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = with inputs; [
+            devshell.overlay
+          ];
+        };
+      in {
+        devShells.default = pkgs.devshell.mkShell {
+          packages = with pkgs; [
+            cue
+            cuelsp
+          ];
+        };
+      }
+    );
+}
